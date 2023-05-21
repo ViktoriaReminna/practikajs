9705170bbb74fb2eb41a4df7d1dddaaa6cad8079
AlexBabanskiy <124041034+AlexBabanskiy@users.noreply.github.com>
2023-05-21 12:01:48 +0200

    Super work

--- js04.js
+++ js04.js
@@ -72,3 +72,109 @@
 // const biscuit = new Client("pixie", "pixie@mail.com");
 // console.log((biscuit.login = "dandelion"));
 // console.log(biscuit.clientData);
+
+//TODO:======================
+// Створи клас для калькулятора, який має такі методи:
+// метод number, який набуває початкового значення для наступних операцій
+// Геттер, який повертає фінальний результат усіх операцій, проведених із числом
+// методи add, substruct, divide, multiply
+
+// class Calc {
+//   constructor() {
+//     this.number = 0;
+//   }
+
+//   get finalNumber() {
+//     return this.number;
+//   }
+
+//   add(number) {
+//     return (this.number += number);
+//   }
+
+//   subs(number) {
+//     return (this.number -= number);
+//   }
+//   divide(number) {
+//     if (number !== 0) {
+//       return (this.number /= number);
+//     } else {
+//       return `На нуль неможна делити!`;
+//     }
+//   }
+
+//   multiply(number) {
+//     return (this.number *= number);
+//   }
+// }
+
+// const calc = new Calc();
+
+// calc.add(5);
+// calc.subs(3);
+// calc.divide(2);
+// calc.multiply(22);
+
+// console.log(calc.finalNumber);
+
+//TODO:======================
+//  Написати клас, який буде представляти зоопарк тварин.
+//  Створити загальний клас Animal для тварин.
+// У зоопарку повинні бути різні типи тварин, такі як ссавці, птахи, рептилії тощо.
+// Кожен тип тварин повинен мати свої властивості та методи, наприклад,
+//  методи для отримання інформації про тварину та для годівлі тварин.
+//  Крім того, зоопарк повинен мати метод для додавання тварин до списку та
+// метод для виведення списку всіх тварин у зоопарку.
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  getInfo() {
+    return `name: ${this.name}, type: ${this.type}`;
+  }
+}
+class Mammal extends Animal {
+  constructor(name, type, ability) {
+    super(name, type);
+    this.ability = ability;
+  }
+
+  getInfo() {
+    return `${super.getInfo()}, ability: ${this.ability}`;
+  }
+}
+
+const elephant = new Mammal(
+  "Bimba",
+  "african elephant",
+  "can reach food with his trunk"
+);
+
+const lion = new Mammal(
+  "Simba",
+  "african lion",
+  "can reach food with his teeth"
+);
+
+class Zoo {
+  constructor() {
+    this.animals = [];
+  }
+
+  addAnimal(newAnimal) {
+    this.animals.push(newAnimal);
+  }
+
+  allAnimals() {
+    this.animals.forEach((animal) => console.log(animal.getInfo()));
+  }
+}
+
+const zoo = new Zoo();
+zoo.addAnimal(elephant);
+zoo.addAnimal(lion);
+
+zoo.allAnimals();
